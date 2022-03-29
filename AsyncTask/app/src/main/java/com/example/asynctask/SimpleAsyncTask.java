441c62b5405c23586619a249034efe673d274a95
irfan-wahid <80728783+irfan-wahid@users.noreply.github.com>
2022-03-29 09:15:21 +0700

    Add files via upload

---
+++ AsyncTask/app/src/main/java/com/example/asynctask/SimpleAsyncTask.java
@@ -0,0 +1,40 @@
+package com.example.asynctask;
+
+import android.os.AsyncTask;
+import android.widget.TextView;
+
+import java.lang.ref.WeakReference;
+import java.util.Random;
+
+public class SimpleAsyncTask extends AsyncTask<Void,Void, String> {
+
+    // The TextView where we will show results
+    private WeakReference<TextView> mTextView;
+
+    // Constructor that provides a reference to the TextView from the MainActivity
+    SimpleAsyncTask(TextView tv) {
+        mTextView = new WeakReference<>(tv);
+    }
+
+    @Override
+    protected String doInBackground(Void... voids) {
+
+        Random r = new Random();
+        int n = r.nextInt(11);
+
+        int s = n * 200;
+
+        try {
+            Thread.sleep(s);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Return a String result.
+        return "Awake at last after sleeping for " + s + " milliseconds!";
+    }
+
+    protected void onPostExecute(String result) {
+        mTextView.get().setText(result);
+    }
+}
